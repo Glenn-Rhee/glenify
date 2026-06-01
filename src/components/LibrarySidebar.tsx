a84f0cbdf1090f133fcc0cc5be0d2cbdd2534b96
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-01 19:36:43 +0700

    feat: added link at list song

--- src/components/LibrarySidebar.tsx
+++ src/components/LibrarySidebar.tsx
@@ -12,16 +12,19 @@ import {
 } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
-import { PlusIcon, SearchIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { PauseIcon, PlayIcon, PlusIcon, SearchIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import DropdownShortLibrary from "./DropdownShortLibrary";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LibrarySidebar() {
   const pathname = usePathname();
   const { open } = useSidebar();
   const [openSearch, setOpenSearch] = useState(false);
+  const [isplaying, setIsPlaying] = useState(false);
+  const router = useRouter();
 
   if (pathname.includes("/auth")) return null;
   return (
@@ -80,21 +83,44 @@ export default function LibrarySidebar() {
         <SidebarGroup>
           <SidebarMenu className={cn(open ? "gap-y-4" : "gap-y-2")}>
             {Array.from({ length: 50 }).map((_, i) => (
-              <SidebarMenuItem key={i} className="flex items-center gap-x-4">
-                <Image
-                  src={"/dummy-prof.jpg"}
-                  alt="Image of Library"
-                  width={60}
-                  height={60}
-                  className="aspect-square rounded-md object-cover"
-                />
-                {open ? (
-                  <div>
-                    <h4 className="font-semibold">Taylorrrrrr</h4>
-                    <span className="text-sm">Playlist | Glenn Rhee</span>
-                  </div>
-                ) : null}
-              </SidebarMenuItem>
+              <div
+                key={i}
+                onClick={() => router.push("/library/1")}
+                role="button"
+                className="hover:bg-muted px-1.5 py-2 rounded-md group/item"
+              >
+                <SidebarMenuItem className="flex items-center gap-x-4">
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setIsPlaying(!isplaying);
+                    }}
+                    className="relative z-10 cursor-pointer"
+                  >
+                    <Image
+                      src={"/dummy-prof.jpg"}
+                      alt="Image of Library"
+                      width={60}
+                      height={60}
+                      className="aspect-square rounded-md object-cover"
+                    />
+                    <span className="absolute cursor-pointer z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-opacity-50 rounded-full p-1 opacity-0 group-hover/item:opacity-100 transition-opacity">
+                      {isplaying ? (
+                        <PauseIcon className="size-6" />
+                      ) : (
+                        <PlayIcon className="size-6" />
+                      )}
+                    </span>
+                    <div className="absolute z-20 top-0 right-0 left-0 bottom-0 bg-gray-700/25 rounded-md opacity-0 transition-all group-hover/item:opacity-100" />
+                  </button>
+                  {open ? (
+                    <div>
+                      <h4 className="font-semibold">Taylorrrrrr</h4>
+                      <span className="text-sm">Playlist | Glenn Rhee</span>
+                    </div>
+                  ) : null}
+                </SidebarMenuItem>
+              </div>
             ))}
           </SidebarMenu>
         </SidebarGroup>
