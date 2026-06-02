311b3f6daf0fd894fb9523f1dbe259854f49636c
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-02 12:20:49 +0700

    refactor: refactoring context menu song

---
+++ src/components/ContextMenuSong.tsx
@@ -0,0 +1,34 @@
+import SongMenuItems from "./SongMenuItems";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuGroup,
+  ContextMenuItem,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "./ui/context-menu";
+
+interface ContextMenuSongProps {
+  children: React.ReactNode;
+}
+
+const ContextMap = {
+  Item: ContextMenuItem,
+  Sub: ContextMenuSub,
+  SubTrigger: ContextMenuSubTrigger,
+  SubContent: ContextMenuSubContent,
+  Group: ContextMenuGroup,
+};
+export default function ContextMenuSong(props: ContextMenuSongProps) {
+  const { children } = props;
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger>{children}</ContextMenuTrigger>
+      <ContextMenuContent className="w-64 px-2 py-1.5 rounded-sm! space-y-2">
+        <SongMenuItems menu={ContextMap} />
+      </ContextMenuContent>
+    </ContextMenu>
+  );
+}
--- src/components/QueueSidebar.tsx
+++ src/components/QueueSidebar.tsx
@@ -8,31 +8,12 @@ import {
   useSidebar,
 } from "./ui/sidebar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import DropdownMenuSong from "./DropdownMenuSong";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuGroup,
-  ContextMenuItem,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
-} from "./ui/context-menu";
-import SongMenuItems from "./SongMenuItems";
 import ImageTriggerSong from "./ImageTriggerSong";
 import ContainerSong from "./ContainerSong";
-
-const ContextMap = {
-  Item: ContextMenuItem,
-  Sub: ContextMenuSub,
-  SubTrigger: ContextMenuSubTrigger,
-  SubContent: ContextMenuSubContent,
-  Group: ContextMenuGroup,
-};
+import ContextMenuSong from "./ContextMenuSong";
 
 export default function QueueSidebar() {
   const { open } = useSidebar();
@@ -58,76 +39,66 @@ export default function QueueSidebar() {
           <SidebarContent className="px-3">
             <TabsContent value="queue" className="mt-4">
               <h4 className="font-semibold text-lg">Now Playing</h4>
-              <ContextMenu>
-                <ContextMenuTrigger asChild>
-                  <ContainerSong
-                    asChild
-                    className="flex items-center justify-between mt-2"
-                  >
-                    <div className="flex items-center gap-x-2">
-                      <ImageTriggerSong
-                        src="/dummy-prof.jpg"
-                        href="/"
-                        size={50}
-                        isplaying={isPlaying}
-                        setIsPlaying={setIsPlaying}
-                      />
-                      <div>
-                        <h4 className="font-medium truncate">
-                          I Love You But Im letting Go
-                        </h4>
-                        <Link
-                          href={"/artist/1"}
-                          className="truncate text-sm hover:underline"
-                        >
-                          Taylor Swift
-                        </Link>
-                      </div>
+              <ContextMenuSong>
+                <ContainerSong
+                  asChild
+                  className="flex items-center justify-between mt-2"
+                >
+                  <div className="flex items-center gap-x-2">
+                    <ImageTriggerSong
+                      src="/dummy-prof.jpg"
+                      href="/"
+                      size={50}
+                      isplaying={isPlaying}
+                      setIsPlaying={setIsPlaying}
+                    />
+                    <div>
+                      <h4 className="font-medium truncate">
+                        I Love You But Im letting Go
+                      </h4>
+                      <Link
+                        href={"/artist/1"}
+                        className="truncate text-sm hover:underline"
+                      >
+                        Taylor Swift
+                      </Link>
                     </div>
-                    <DropdownMenuSong />
-                  </ContainerSong>
-                </ContextMenuTrigger>
-                <ContextMenuContent className="w-64 px-2 py-1.5 rounded-sm! space-y-2">
-                  <SongMenuItems menu={ContextMap} />
-                </ContextMenuContent>
-              </ContextMenu>
+                  </div>
+                  <DropdownMenuSong />
+                </ContainerSong>
+              </ContextMenuSong>
               <h4 className="font-semibold text-lg mt-4">Next</h4>
               <div className="flex flex-col gap-y-2">
                 {Array.from({ length: 10 }).map((_, i) => (
-                  <ContextMenu key={i}>
-                    <ContextMenuTrigger>
-                      <ContainerSong
-                        key={i}
-                        asChild
-                        className="flex items-center justify-between mt-2"
-                      >
-                        <div className="flex items-center gap-x-2">
-                          <ImageTriggerSong
-                            src="/dummy-prof.jpg"
-                            href="/"
-                            size={50}
-                            isplaying={isPlaying}
-                            setIsPlaying={setIsPlaying}
-                          />
-                          <div>
-                            <h5 className="font-medium truncate">
-                              I Love You But Im letting Go
-                            </h5>
-                            <Link
-                              href={"/artist/1"}
-                              className="truncate text-sm hover:underline"
-                            >
-                              Taylor Swift
-                            </Link>
-                          </div>
+                  <ContextMenuSong key={i}>
+                    <ContainerSong
+                      key={i}
+                      asChild
+                      className="flex items-center justify-between mt-2"
+                    >
+                      <div className="flex items-center gap-x-2">
+                        <ImageTriggerSong
+                          src="/dummy-prof.jpg"
+                          href="/"
+                          size={50}
+                          isplaying={isPlaying}
+                          setIsPlaying={setIsPlaying}
+                        />
+                        <div>
+                          <h5 className="font-medium truncate">
+                            I Love You But Im letting Go
+                          </h5>
+                          <Link
+                            href={"/artist/1"}
+                            className="truncate text-sm hover:underline"
+                          >
+                            Taylor Swift
+                          </Link>
                         </div>
-                        <DropdownMenuSong />
-                      </ContainerSong>
-                    </ContextMenuTrigger>
-                    <ContextMenuContent className="w-64 px-2 py-1.5 rounded-sm! space-y-2">
-                      <SongMenuItems menu={ContextMap} />
-                    </ContextMenuContent>
-                  </ContextMenu>
+                      </div>
+                      <DropdownMenuSong />
+                    </ContainerSong>
+                  </ContextMenuSong>
                 ))}
               </div>
             </TabsContent>
@@ -136,21 +107,29 @@ export default function QueueSidebar() {
               className="mt-4 flex flex-col gap-y-4"
             >
               {Array.from({ length: 10 }).map((_, i) => (
-                <div key={i} className="flex items-center gap-x-2 mt-2">
-                  <Image
-                    src={"/dummy-prof.jpg"}
-                    alt="Picture of song"
-                    width={50}
-                    height={50}
-                    className="aspect-square rounded-md object-cover"
-                  />
-                  <div>
-                    <h5 className="font-medium truncate">
-                      I Love You But Im letting Go
-                    </h5>
-                    <span className="truncate text-sm">Taylor Swift</span>
-                  </div>
-                </div>
+                <ContextMenuSong key={i}>
+                  <ContainerSong
+                    asChild
+                    className="flex items-center justify-between mt-2"
+                  >
+                    <div key={i} className="flex items-center gap-x-2">
+                      <ImageTriggerSong
+                        src="/dummy-prof.jpg"
+                        href="/"
+                        size={50}
+                        isplaying={isPlaying}
+                        setIsPlaying={setIsPlaying}
+                      />
+                      <div>
+                        <h5 className="font-medium truncate">
+                          I Love You But Im letting Go
+                        </h5>
+                        <span className="truncate text-sm">Taylor Swift</span>
+                      </div>
+                    </div>
+                    <DropdownMenuSong />
+                  </ContainerSong>
+                </ContextMenuSong>
               ))}
             </TabsContent>
           </SidebarContent>
