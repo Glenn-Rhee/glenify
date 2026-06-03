cad6050712a5fa29cc4a13f76d7598cddacc8637
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-03 21:15:14 +0700

    feat: adeded search icon at search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
 "use client";
-import { House } from "lucide-react";
-import Image from "next/image";
+import { House, SearchIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Input } from "./ui/input";
 import DropdownmenuProfile from "./dropdownmenu/DropdownmenuProfile";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Header() {
+  const [isSearchFocus, setIsSearchFocus] = useState<boolean>(false);
   const pathname = usePathname();
   return (
     <header className="h-18 sticky top-0 left-0 right-0 z-50 shrink-0 px-10 flex items-center justify-between bg-[#0b0f18]">
-      <Link href={"/"} className="font-bold text-2xl">Glenifyy</Link>
-      <div className="flex items-center gap-x-2">
+      <Link href={"/"} className="font-bold text-2xl">
+        Glenifyy
+      </Link>
+      <div className="flex items-center gap-x-2 h-full">
         <Link
           href={"/"}
           className="aspect-square flex items-center justify-center rounded-full size-12 bg-gray-100/35"
@@ -22,7 +25,20 @@ export default function Header() {
             <House className="size-6" />
           )}
         </Link>
-        <Input className="w-124" />
+        <div
+          className={cn(
+            "flex items-center gap-x-2 bg-gray-100/10 h-[calc(100%-1.3rem)] px-2 rounded-xl group/search transition-colors",
+            isSearchFocus ? "border-ring ring-2 ring-white/50 inset-0.5" : "",
+          )}
+        >
+          <SearchIcon className="size-6" />
+          <input
+            placeholder="What do you want to play?"
+            onFocus={() => setIsSearchFocus(true)}
+            onBlur={() => setIsSearchFocus(false)}
+            className="w-124 bg-transparent focus-visible:border-ring focus:outline-none placeholder:text-sm transition-all"
+          />
+        </div>
       </div>
       <div className="flex items-center gap-x-2">
         <DropdownmenuProfile />
