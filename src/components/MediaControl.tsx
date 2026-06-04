3abafe85dbdf61443b6ca2410bdd7a6750523390
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-04 13:50:19 +0700

    feat: added song section

--- src/components/MediaControl.tsx
+++ src/components/MediaControl.tsx
@@ -10,9 +10,9 @@ export default function MediaControl(props: MediControlProps) {
     <>
       <span className="absolute cursor-pointer z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-opacity-50 rounded-full p-1 opacity-0 group-hover/item:opacity-100 transition-opacity">
         {isPlaying ? (
-          <PauseIcon className="size-6" />
+          <PauseIcon className="size-6" fill="white"/>
         ) : (
-          <PlayIcon className="size-6" />
+          <PlayIcon className="size-6" fill="white" />
         )}
       </span>
       <div className="absolute z-20 top-0 right-0 left-0 bottom-0 bg-gray-700/25 rounded-md opacity-0 transition-all group-hover/item:opacity-100" />
--- src/components/pages/home/AllTabs.tsx
+++ src/components/pages/home/AllTabs.tsx
@@ -1,11 +1,13 @@
 import AlbumSection from "./AlbumSection";
 import ArtistSection from "./ArtistSection";
+import SongSection from "./SongSection";
 
 export default function AllTabs() {
   return (
     <div className="flex flex-col gap-y-4">
       <ArtistSection />
       <AlbumSection />
+      <SongSection />
     </div>
   );
 }
