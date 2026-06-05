2fba0e34eafa1c32c29b4782f83939ccd8cedb7a
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-05 12:55:23 +0700

    fix: fixed gap action artist

--- src/components/pages/album/MainContent.tsx
+++ src/components/pages/album/MainContent.tsx
@@ -5,7 +5,7 @@ import { MoreVertical, PlayIcon, PlusCircle, ShuffleIcon } from "lucide-react";
 export default function MainContent() {
   return (
     <Container className="space-y-2 pb-56">
-      <div className="flex items-center gap-x-4 w-full my-4">
+      <div className="flex items-center gap-x-8 w-full my-4">
         <button className="size-12 bg-primary rounded-full flex items-center justify-center">
           <PlayIcon className="size-6" fill="white" />
         </button>
