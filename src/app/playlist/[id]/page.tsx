634c14ff1b2416c3a711dd5a78c0ce3b36f68f32
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-09 23:10:20 +0700

    feat: modified playlist page id

--- src/app/playlist/[id]/page.tsx
+++ src/app/playlist/[id]/page.tsx
@@ -1,12 +1,14 @@
+import MainContent from "@/components/pages/album/MainContent";
 import BannerPlaylist from "@/components/pages/playlist/BannerPlaylist";
 
-export default function LibraryPage() {
+export default function PlaylistPage() {
   return (
     <div
       id="library-page"
       className="w-full relative overflow-x-hidden overflow-y-auto no-scrollbar"
     >
-      <BannerPlaylist />
+      <BannerPlaylist elementId="library-page" />
+      <MainContent useFor="playlist" />
     </div>
   );
 }
