5f34a23cebb847c07681bade410191537acd7631
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-09 23:11:31 +0700

    feat: modified searchghost

--- src/components/SearchGhost.tsx
+++ src/components/SearchGhost.tsx
@@ -4,7 +4,13 @@ import { Button } from "./ui/button";
 import { SearchIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-export default function SearchGhost() {
+interface SearchGhostProps {
+  direction?: "left" | "right";
+  placeholder?: string;
+}
+
+export default function SearchGhost(props: SearchGhostProps) {
+  const { direction = "right", placeholder } = props;
   const [openSearch, setOpenSearch] = useState(false);
   return (
     <div className="flex items-center gap-2">
@@ -20,8 +26,9 @@ export default function SearchGhost() {
         className={cn(
           "w-56 bg-transparent border-b border-white focus-visible:border-ring focus:ring-0 focus:outline-none placeholder:text-sm transition-all",
           openSearch ? "w-56" : "w-0",
+          direction === "left" ? "order-first" : "order-last",
         )}
-        placeholder="Search your library..."
+        placeholder={placeholder}
       />
     </div>
   );
