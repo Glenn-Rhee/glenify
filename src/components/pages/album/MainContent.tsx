8a3a06130ea36c8f450e2c1a03f1a210767b1f9b
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-10 21:08:19 +0700

    feat: added main content

--- src/components/pages/album/MainContent.tsx
+++ src/components/pages/album/MainContent.tsx
@@ -2,23 +2,38 @@ import Container from "@/components/Container";
 import DropdownmenuAlbum from "@/components/dropdownmenu/DropdownmenuAlbum";
 import { MoreVertical, PlayIcon, PlusCircle, ShuffleIcon } from "lucide-react";
 import DataTableSong from "./DataTableSong";
+import SearchGhost from "@/components/SearchGhost";
+import DropdownmenuShortPlaylist from "@/components/dropdownmenu/DropdownmenuShortPlaylist";
 
-export default function MainContent() {
+interface MainContentProps {
+  useFor?: "playlist" | "album";
+}
+
+export default function MainContent(props: MainContentProps) {
+  const { useFor = "album" } = props;
   return (
     <Container className="space-y-2 pb-56">
-      <div className="flex items-center gap-x-8 w-full my-4">
-        <button className="size-12 bg-primary rounded-full flex items-center justify-center">
-          <PlayIcon className="size-6" fill="white" />
-        </button>
-        <button>
-          <ShuffleIcon className="size-6" />
-        </button>
-        <button>
-          <PlusCircle className="size-6" />
-        </button>
-        <DropdownmenuAlbum>
-          <MoreVertical className="size-6" />
-        </DropdownmenuAlbum>
+      <div className="flex items-center justify-between gap-x-8 w-full my-4">
+        <div className="flex items-center gap-x-8 w-full">
+          <button className="size-12 bg-primary rounded-full flex items-center justify-center">
+            <PlayIcon className="size-6" fill="white" />
+          </button>
+          <button>
+            <ShuffleIcon className="size-6" />
+          </button>
+          <button>
+            <PlusCircle className="size-6" />
+          </button>
+          <DropdownmenuAlbum>
+            <MoreVertical className="size-6" />
+          </DropdownmenuAlbum>
+        </div>
+        {useFor === "playlist" && (
+          <div className="flex items-center gap-x-4">
+            <SearchGhost placeholder="Find in playlist" direction="left" />
+            <DropdownmenuShortPlaylist />
+          </div>
+        )}
       </div>
       <DataTableSong />
     </Container>
