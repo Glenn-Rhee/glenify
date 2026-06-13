13bf6a254293ed5157e2553235fe6bd847c87f8e
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-13 14:13:52 +0700

    feat: added profile banner

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import BannerProfile from "@/components/pages/profile/BannerProfile";
+
+export default function ProfilePage() {
+  return (
+    <div
+      id="profile-page"
+      className="w-full relative overflow-x-hidden overflow-y-auto no-scrollbar"
+    >
+      <BannerProfile />
+    </div>
+  );
+}
---
+++ src/components/pages/profile/BannerProfile.tsx
@@ -0,0 +1,44 @@
+"use client";
+import BannerContainer from "@/components/BannerContainer";
+import HeaderBanner from "@/components/HeaderBanner";
+import { Pencil } from "lucide-react";
+import Image from "next/image";
+import { useRef } from "react";
+
+export default function BannerProfile() {
+  const triggerRef = useRef<HTMLHeadingElement | null>(null);
+
+  return (
+    <>
+      <HeaderBanner
+        title="Ariel Rizki"
+        elementId="profile-page"
+        triggerRef={triggerRef}
+      />
+      <BannerContainer
+        ref={triggerRef}
+        className="h-[30dvh] bg-orange-800 flex px-3 py-2 gap-x-8 items-end"
+      >
+        <button className="relative cursor-pointer group/profile">
+          <Image
+            src={"/dummy-prof.jpg"}
+            alt="Profile Picture"
+            width={180}
+            height={180}
+            className="object-cover aspect-square rounded-full shadow-xl"
+          />
+          <div className="absolute group-hover/profile:opacity-100 transition-all duration-300 opacity-0 top-0 right-0 left-0 bg-sidebar/30 h-full w-full rounded-full flex items-center justify-center">
+            <Pencil className="size-8" />
+          </div>
+        </button>
+        <div className="flex flex-col gap-y-1.5">
+          <span>Profile</span>
+          <button>
+            <h1 className="font-bold text-7xl">Glenn</h1>
+          </button>
+          <span className="font-medium text-lg">10 Public Playlist</span>
+        </div>
+      </BannerContainer>
+    </>
+  );
+}
