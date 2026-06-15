08c13670dc1d019a38c668bf984bcb9f746b1f4c
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-15 18:58:42 +0700

    feat: added conditional rendering

--- src/components/ProfileMenuItems.tsx
+++ src/components/ProfileMenuItems.tsx
@@ -1,14 +1,28 @@
-import { Copy } from "lucide-react";
+"use client";
+import { Copy, PencilIcon } from "lucide-react";
 import { ItemsOverlay } from "./SongMenuItems";
+import DialogUpdateProfile from "./dialog/DialogUpdateProfile";
 
 interface ProfileMenuItems {
   menu: ItemsOverlay;
+  isForOption?: boolean;
 }
 export default function ProfileMenuItems(props: ProfileMenuItems) {
-  const { menu } = props;
+  const { menu, isForOption = false } = props;
   return (
-    <menu.Item className="flex items-center gap-x-3">
-      <Copy className="size-4" /> Copy link profile
-    </menu.Item>
+    <>
+      {isForOption ? (
+        <menu.Item>
+          <DialogUpdateProfile>
+            <button className="flex items-center gap-x-3">
+              <PencilIcon className="size-4" /> Edit Profile
+            </button>
+          </DialogUpdateProfile>
+        </menu.Item>
+      ) : null}
+      <menu.Item className="flex items-center gap-x-3">
+        <Copy className="size-4" /> Copy link profile
+      </menu.Item>
+    </>
   );
 }
