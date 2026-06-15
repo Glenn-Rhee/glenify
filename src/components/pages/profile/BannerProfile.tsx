16493ae3d46ae48891af7587ac406e82a159a68f
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-15 18:30:54 +0700

    feat: added dialogupdate profile

--- src/components/pages/profile/BannerProfile.tsx
+++ src/components/pages/profile/BannerProfile.tsx
@@ -22,20 +22,19 @@ export default function BannerProfile() {
         className="h-[30dvh] bg-orange-800 flex px-3 py-2 gap-x-8 items-end"
       >
         <DialogUpdateProfile>
-          
+          <button className="relative cursor-pointer group/profile">
+            <Image
+              src={"/dummy-prof.jpg"}
+              alt="Profile Picture"
+              width={180}
+              height={180}
+              className="object-cover aspect-square rounded-full shadow-xl"
+            />
+            <div className="absolute group-hover/profile:opacity-100 transition-all duration-300 opacity-0 top-0 right-0 left-0 bg-sidebar/30 h-full w-full rounded-full flex items-center justify-center">
+              <Pencil className="size-8" />
+            </div>
+          </button>
         </DialogUpdateProfile>
-        <button className="relative cursor-pointer group/profile">
-          <Image
-            src={"/dummy-prof.jpg"}
-            alt="Profile Picture"
-            width={180}
-            height={180}
-            className="object-cover aspect-square rounded-full shadow-xl"
-          />
-          <div className="absolute group-hover/profile:opacity-100 transition-all duration-300 opacity-0 top-0 right-0 left-0 bg-sidebar/30 h-full w-full rounded-full flex items-center justify-center">
-            <Pencil className="size-8" />
-          </div>
-        </button>
         <div className="flex flex-col gap-y-1.5">
           <span>Profile</span>
           <ContextMenuProfile>
