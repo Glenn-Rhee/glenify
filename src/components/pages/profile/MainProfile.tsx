bbee38e4bd30128fcf50c30a09d177968535d261
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-15 18:59:04 +0700

    feat: added mainprofile.tsx

---
+++ src/components/pages/profile/MainProfile.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+import DropdownmenuMoreOptProfile from "@/components/dropdownmenu/DropdownmenuMoreOptProfile";
+import { MoreHorizontal } from "lucide-react";
+
+export default function MainProfile() {
+  return (
+    <Container className="space-y-2 pb-56">
+      <DropdownmenuMoreOptProfile>
+        <MoreHorizontal className="size-6" />
+      </DropdownmenuMoreOptProfile>
+    </Container>
+  );
+}
