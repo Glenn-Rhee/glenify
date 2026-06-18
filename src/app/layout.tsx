c4c813844d6a8b945c9e9b6c3678476d0a3e1a50
Ariel Rizki <135257091+Glenn-Rhee@users.noreply.github.com>
2026-06-18 21:25:19 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "Glenify Music Platform",
-  description: "Glenify",
+  description: "Glenif",
 };
 
 export default function RootLayout({
