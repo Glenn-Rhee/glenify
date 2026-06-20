9e5623aca396079f69a2599ddb3e573de3ec3fef
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-20 11:20:00 +0700

    feat: add play button at image artist

--- src/components/pages/profile/SectionArtist.tsx
+++ src/components/pages/profile/SectionArtist.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { PlayIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -24,15 +25,20 @@ export default function SectionArtist() {
           <Link
             key={i}
             href={"/artist/1"}
-            className="flex flex-col justify-self-center items-center justify-center w-full max-w-32 gap-y-1 hover:bg-white/10 transition-colors py-1.5 rounded-md"
+            className="flex flex-col group/artist justify-self-center items-center justify-center w-full max-w-32 gap-y-1 hover:bg-white/10 transition-colors py-1.5 rounded-md"
           >
-            <Image
-              src={"/dummy-prof.jpg"}
-              alt="Profile pict Artiist"
-              width={110}
-              height={110}
-              className="aspect-square object-cover rounded-full"
-            />
+            <div className="relative">
+              <Image
+                src={"/dummy-prof.jpg"}
+                alt="Profile pict Artiist"
+                width={110}
+                height={110}
+                className="aspect-square object-cover rounded-full"
+              />
+              <button className="size-10 group-hover/artist:opacity-100 opacity-0 transition-all absolute bottom-2 -right-2 bg-primary flex items-center justify-center rounded-full">
+                <PlayIcon fill="white" className="size-6" />
+              </button>
+            </div>
             <span className="font-semibold text-center text-xl truncate w-full">
               Taylor Swift
             </span>
