2a04ebe96ee16b9782e5b75471c405ff81e67821
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-20 19:27:19 +0700

    feat: addded tabs all at search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,53 @@
+import Container from "@/components/Container";
+import Alltabs from "@/components/pages/search/Alltabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 interface SearchPageProps {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 export default async function SearchPage(props: SearchPageProps) {
   const { searchParams } = props;
-  const query = await searchParams
-  console.log(query)
+  const query = await searchParams;
 
   return (
-    <div>cihuy</div>
-  )
+    <Container>
+      <Tabs defaultValue="all" className="mt-4">
+        <TabsList className="bg-background max-w-108 w-full gap-x-4 mb-2 sticky top-2 left-0 right-0 z-80">
+          <TabsTrigger
+            value="all"
+            className="dark:data-active:bg-white/90 dark:data-active:text-background dark:data-active:hover:text-background px-3"
+          >
+            All
+          </TabsTrigger>
+          <TabsTrigger
+            className="dark:data-active:bg-white/90 dark:data-active:text-background dark:data-active:hover:text-background px-3"
+            value="song"
+          >
+            Song
+          </TabsTrigger>
+          <TabsTrigger
+            className="dark:data-active:bg-white/90 dark:data-active:text-background dark:data-active:hover:text-background px-3"
+            value="album"
+          >
+            Album
+          </TabsTrigger>
+          <TabsTrigger
+            className="dark:data-active:bg-white/90 dark:data-active:text-background dark:data-active:hover:text-background px-3"
+            value="playlist"
+          >
+            Playlist
+          </TabsTrigger>
+          <TabsTrigger
+            className="dark:data-active:bg-white/90 dark:data-active:text-background dark:data-active:hover:text-background px-3"
+            value="artist"
+          >
+            Artist
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <Alltabs />
+        </TabsContent>
+      </Tabs>
+    </Container>
+  );
 }
