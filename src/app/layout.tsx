4e95afed68b8b307e37d1400c69fc88f4f83b816
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-25 23:37:35 +0700

    feat: updaet metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   title: "Glenify Music Platform",
   description:
     "Glenify is a music streaming platform where you can discover, listen, and enjoy music from artists around the world.",
+  keywords: ["music", "streaming", "music platform", "listen music", "glenify"],
 };
 
 export default function RootLayout({
