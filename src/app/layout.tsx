f70c2808ec0e666c6e7fbe268f887191e9cbfa63
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-26 23:24:42 +0700

    feat: update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,7 @@ export const metadata: Metadata = {
     url: "https://glenify.com",
     siteName: "Glenify",
     images: [
-      {
-        url: "https://glenify.com/og-image.png", // ganti dengan URL gambar OG kamu
-        width: 1200,
-        height: 630,
-        alt: "Glenify Music Platform",
-      },
+      
     ],
   },
 };
