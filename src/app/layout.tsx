694db7eb20a4d8b4f89017cc1bb556b6d9e27b93
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-27 21:29:54 +0700

    refactor: update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
-  title: "Glenify Music Platform",
+  title: "Glenify Music ",
   description:
     "Glenify is a music streaming platform where you can discover, listen, and enjoy music from artists around the world.",
   keywords: ["music", "streaming", "music platform", "listen music", "glenify"],
